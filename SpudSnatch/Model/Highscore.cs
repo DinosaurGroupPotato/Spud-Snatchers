d1caede68953d14f23bc384e4b05399abeac1dc0
Guest PC <Guest PC@DESKTOP-QFE29KU>
2017-03-31 18:44:24 -0400

    Highscore class

---
+++ SpudSnatch/Model/Highscore.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpudSnatch.Model
+{
+    class Highscore
+    {
+        public static Dictionary<string, int> highscores = new Dictionary<string, int>();
+
+        public static void AddScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void ManageScores()
+        {
+            ///keeps track of the number of high scores
+            ///only allows a certian number in the dictionary
+            throw new NotImplementedException();
+        }
+    }
+}
