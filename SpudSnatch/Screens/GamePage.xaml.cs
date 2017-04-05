512f94b73952f42edefb61981e8227fa4e4d9afb
rlong315 <rlong315@students.bju.edu>
2017-04-04 20:55:31 -0400

    Added a timer to GamePage.

--- SpudSnatch/Screens/GamePage.xaml.cs
+++ SpudSnatch/Screens/GamePage.xaml.cs
@@ -1,30 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using SpudSnatch.Model;
 
 namespace SpudSnatch
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class GamePage : Page
     {
+
+        // Instance variables for the GameController
+        private GameController ctrl;
+
+        // Timer to keep track of updates
+        DispatcherTimer timer;
+
         public GamePage()
         {
             this.InitializeComponent();
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
